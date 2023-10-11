d86e567431156a4449cb313b7785ee9ee50ba585
RaiusGames <121724863+RaiusGames@users.noreply.github.com>
2023-10-11 11:48:05 +0100

    make repository

---
+++ age-verification.js
@@ -0,0 +1,11 @@
+overAge = function ()
+{
+    sessionStorage.setItem("overage", true);
+    //window.history.back();
+    window.location.href = "index.html";
+}
+
+goBack = function ()
+{
+    window.location.href = "https://www.google.com";
+}
