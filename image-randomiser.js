9b1af9b47f30ce694be2c6ac7681bda4ead0162b
RaiusGames <121724863+RaiusGames@users.noreply.github.com>
2024-02-03 13:51:14 +0900

    aaaa

---
+++ image-randomiser.js
@@ -0,0 +1,27 @@
+const images =
+[
+    'url("images/witch.jpg")',
+    'url("images/ambush.jpg")',
+    'url("images/late-night.jpg")',
+    'url("images/rain.jpg")',
+    'url("images/games/3d-test1.jpg")',
+    'url("images/games/3d-test2.jpg")',
+    'url("images/games/strange-shores2.jpg")',
+];
+//const bg = document.getElementById("background").style.backgroundImage;
+var currIndex = 0;
+
+function showImage()
+{
+    //var a = Math.floor(Math.random() * pics.length); 
+    currIndex++;
+    if(currIndex >= images.length)
+    {
+        currIndex = 0;
+    }
+    var currImage = images[currIndex];
+    //pic.style.backgroundImage = img;
+    var bg = document.getElementById("background").style.backgroundImage = currImage;
+} 
+
+setInterval(showImage, 3500);
