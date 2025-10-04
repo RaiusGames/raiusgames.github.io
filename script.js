33730a555df459db569dc7c695fd2ebb9c11dd79
RaiusGames <RaiusGames@hotmail.com>
2025-10-04 15:43:47 +0900

    added fade and movement animations

---
+++ script.js
@@ -0,0 +1,14 @@
+const imageRows = document.querySelectorAll(".image-row");
+
+
+let offset = 0;
+for(const row of imageRows)
+{
+  for(const image of row.children)
+  {
+    let delay = offset * 0.04;
+    image.classList.add("fade");
+    image.style.setProperty("--delayOffset", delay.toString() + "s");
+    offset++;
+  }
+}
